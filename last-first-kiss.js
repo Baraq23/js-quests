48e95792596e25fc08b0dd87c42f5165803a294e
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-26 17:31:34 +0300

    modify: last-first-kiss.js file

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -3,4 +3,4 @@
 const first = (r) => typeof r === "string"? r.charAt(0) : r[0];
 const last = (r) => typeof r === "string"? r.charAt(r.length-1) : r[r.length-1];
 
-const kiss = (arr) => [first(arr), last(arr)];
+const kiss = (arr) => [last(arr), first(arr)];
