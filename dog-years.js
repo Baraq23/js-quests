17478e6c47d0dccf6ae60514308e2585385f5845
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-26 19:39:36 +0300

    modify: dog-years.js file

--- dog-years.js
+++ dog-years.js
@@ -4,32 +4,30 @@ const dogYears = (planet, dogAge) => {
 
     let pl = 0.0;
 
-    switch (planet) {
-        case "earth":
-            pl = 1.0;
-            break;
-        case "mercury":
-            pl = 0.2408467
-            break;
-        case "venus":
-            pl = 0.61519726;
-            break;
-        case "mars":
-            pl = 1.8808158 ;
-            break;
-        case "jupiter":
-            pl = 11.862615;
-            break;
-        case "saturn":
-            pl = 29.447498;
-            break;
-        case "uranus":
-            pl = 84.016846;
-            break;
-        case "neptune":
-            pl = 164.79132;
-            break;
+    if (planet === "earth") {
+        pl = 1.0;
     }
-
-    return Number((((dogAge/31557600)*pl) *7).toFixed(2))
+    if (planet === "mercury") {
+        pl = 0.2408467;
+    }
+    if (planet === "venus") {
+        pl = 0.61519726;
+    }
+    if (planet === "mars") {
+        pl = 1.8808158 ;
+    }
+    if (planet === "jupiter") {
+        pl = 11.862615;
+    }
+    if (planet === "saturn") {
+        pl = 29.447498;
+    }
+    if (planet === "uranus") {
+        pl = 84.016846;
+    }
+    if (planet === "neptune") {
+        pl = 164.79132;
+    }
+    
+    return Number((((dogAge/31557600)/pl) *7).toFixed(2))
 }
