6af081e279931bf27bf4cc1321928fa39e1f8b80
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-26 14:25:53 +0300

    add declaration.js file

---
+++ declarations.js
@@ -0,0 +1,35 @@
+// Variables
+
+// Special characters string
+const escapeStr = "\`\\\/\"\'";
+
+// Array
+const arr = [4, '2'];
+
+// Objects
+const obj = {
+    str: 'string',
+    num: 12,
+    bool: true,
+    undef: undefined
+};
+
+// nested object
+const nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        str: 'nested obj',
+        num: 15,
+        bool: false
+    },
+};
+
+// freezing objects: freezing an object makes the object immutable
+Object.freeze(arr);
+Object.freeze(obj);
+Object.freeze(nested);
+
+// console.log(escapeStr);
+// console.log(arr);
+// console.log(obj);
+// console.log(nested);
