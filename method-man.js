c5509f7c295ea265f5d3df9ca77df739206f4010
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-27 11:38:28 +0300

    feat: declare functions that manupulates string

--- method-man.js
+++ method-man.js
@@ -3,4 +3,4 @@ const words = (s) => s.split(' ');
 const sentence = (s) => s.join(' ');
 const yell = (s) => s.toUpperCase();
 const whisper = (s) => String('*' + s.toLowerCase() + '*');
-const capitalize = (s) => {s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()}; 
+const capitalize = (s) => String(s.charAt(0).toUpperCase() + s.slice(1).toLowerCase());
