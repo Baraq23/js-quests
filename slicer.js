2895c495a09f70c2ba93b40c7a6d9d45f26b85ff
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-30 17:22:24 +0300

    feat: fuction to perform Array.slice() and String.slice()

---
+++ slicer.js
@@ -0,0 +1,30 @@
+const slicer = (arr, s, n) => {
+    if (n !== undefined){
+        let res = []
+        for (i = s; i <= n; i++) {
+            if (arr instanceof Array) {
+                res.push(arr[i])
+            } else {
+                res+= arr[i]
+            }
+        }
+        return res
+    }
+    let res = []
+    for (i = s; i < arr.length; i++) {
+        if (arr instanceof Array) {
+            res.push(arr[i])
+        } else {
+            res+= arr[i]
+        }
+    }
+    return res
+}
+
+// let a = ['a', 'b', 'c', 'd', 'e']
+// let b = ['a', 'b', 'c', 'd', 'e']
+
+// console.log(slicer(a, 3))
+// console.log(slicer(a, 2, 3))
+// console.log(slicer('hellow', 2, 3))
+// console.log(slicer('hellow', 1))
