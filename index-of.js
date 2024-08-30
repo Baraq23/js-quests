c2c4554419db6611228e898ef498a6dd21864f14
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-30 14:18:47 +0300

    feat: funtion that immitates array method; Array.indexOf(), Array.lastIndexOf() and Array.includes()

--- index-of.js
+++ index-of.js
@@ -1,7 +1,7 @@
 
 const indexOf = (arr, item) => {
     // arr.indexOf(item);
-    for (let i = arr.length(); i > 0; i--) {
+    for (let i = arr.length; i > 0; i--) {
         if (item === arr[i]) {
             return i
         }
@@ -12,7 +12,7 @@ const indexOf = (arr, item) => {
 
 const lastIndexOf = (arr, item) => {
     // arr.lastIndexOf(item)
-    for (let i = arr.length(); i > 0; i--) {
+    for (let i = arr.length; i > 0; i--) {
         let indx = -1
         if (item === arr[i]) {
             indx = i
@@ -23,7 +23,7 @@ const lastIndexOf = (arr, item) => {
 
 const includes = (arr, item) => {
     // arr.includes(item)
-    for (let i = arr.length(); i > 0; i--) {
+    for (let i = arr.length; i > 0; i--) {
         if (item === arr[i]) {
             return true
         }
