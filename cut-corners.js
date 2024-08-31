de1fe36456d101a0530b67f1cc714673c4b34e1a
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-31 08:15:05 +0300

    feat: functions tha perform Math.round(), Math.ceil(), Math.floor() and Math.trunc()

--- cut-corners.js
+++ cut-corners.js
@@ -1,5 +1,5 @@
 const modulo = (n, d) => {
-    return n%d
+    return n % d
 }
 
 const round = (n) => {
@@ -10,12 +10,16 @@ const round = (n) => {
     }
 
     let mod = modulo(n, 1)
-    let num = n-mod
-    if (mod >=0.5) {
+    let num = n - mod
+    if (mod >= 0.5) {
         num = num + 1
     }
     if (isNeg) {
-        return -num
+        if (num === 0) {
+            num = num
+        } else {
+            num = -num
+        }
     }
     return num
 }
@@ -28,16 +32,77 @@ const ceil = (n) => {
     }
 
     let mod = modulo(n, 1)
-    let num = n-mod
+    let num = n - mod
     if (mod > 0) {
-        num = num
+        if (isNeg) {
+            if (num === 0) {
+                num = num
+            } else {
+                num = -num
+            }
+        } else {
+            num += 1
+        }
     }
-    if (isNeg) {
-        // num +=1
-        return -num
-    }
-    return num+1
+    return num
 }
+
+const floor = (n) => {
+    let isNeg = false
+    if (n < 0) {
+        n = -n
+        isNeg = true
+    }
+    let mod = modulo(n, 1)
+    let num = n - mod
+
+    if (mod > 0) {
+        if (isNeg) {
+            num += 1
+            if (num === 0) {
+                num = num
+            } else {
+                num = -num
+            }
+        } else {
+            num = num
+        }
+    }
+    return num
+}
+
+const trunc = (n) => {
+    let isNeg = false
+    if (n < 0) {
+        isNeg = true
+        n = -n
+    }
+
+    let mod = modulo(n, 1)
+    let num = n - mod
+    if (isNeg) {
+        if (num === 0) {
+            num = num
+        } else {
+            num = -num
+        }
+    }
+    return num
+}
+
 // console.log(round(-5.6004))
+// console.log(round(-0.6004))
+// console.log(round(-0.01))
 // console.log(ceil(-5.6004))
 // console.log(ceil(0.6004))
+// console.log(ceil(0.0))
+// console.log(floor(0.0))
+// console.log(floor(0.05))
+// console.log(floor(5.05))
+// console.log(floor(5.05))
+// console.log(floor(-1.01))
+// console.log(trunc(0.01))
+// console.log(trunc(-0.01))
+// console.log(trunc(-6.01))
+// console.log(trunc(-7.91))
+// console.log(trunc(3.91))
