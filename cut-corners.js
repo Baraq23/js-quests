e45a666756150e0b007010d774975610828f3885
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-31 09:00:09 +0300

    feat: functions tha perform Math.round(), Math.ceil(), Math.floor() and Math.trunc()

--- cut-corners.js
+++ cut-corners.js
@@ -1,25 +1,54 @@
-// const modulo = (n, d) => {
-//     return n % d
-// }
-const modulo = (a, b) => {
-    // let negA = false
-    // let negB = false
-    // if (a < 0) {
-    //     negA = true
-    //     a = Math.abs(a)
-    // }
-    // if (b < 0) {
-    //     negB = true
-    //     b = Math.abs(b)
-    // }
+const multiply = (a, b) => {
+    let negA = false
+    let negB = false
+    if (a < 0) {
+        negA = true
+        a = Math.abs(a)
+    }
+    if (b < 0) {
+        negB = true
+        b = Math.abs(b)
+    }
+    let results = 0;
+    let count = 0;
+    while (count < b) {
+        results += a
+        count++
+    }
+    if ((negA && negB) || (!negA && !negB)){
+        return results
+    }
+    return -results
+}
+
+const divide = (a, b) => {
+    let negA = false
+    let negB = false
+    if (a < 0) {
+        negA = true
+        a = Math.abs(a)
+    }
+    if (b < 0) {
+        negB = true
+        b = Math.abs(b)
+    }
+
     let results = a;
+    let count = 0;
+
     while (results >= b) {
         results -= b
+        count++
     }
-    // if (negA) {
-    //     return -results
-    // }
-    return results
+
+    if ((negA && negB) || (!negA && !negB)) {
+        return count
+    }
+    return -count
+}
+
+function modulo(num, divisor) {
+    return num - multiply(divisor, divide(num, divisor));
 }
 
 const round = (n) => {
