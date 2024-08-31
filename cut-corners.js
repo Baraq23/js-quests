a33bb7bf11c4b44b9ca0bfdb990fa58872a5d580
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-31 09:59:18 +0300

    feat: functions to perform Math.round(), Math.ceil(), Math.floor() and Math.trunc()

--- cut-corners.js
+++ cut-corners.js
@@ -1,157 +1,95 @@
-const multiply = (a, b) => {
-    let negA = false
-    let negB = false
-    if (a < 0) {
-        negA = true
-        a = Math.abs(a)
-    }
-    if (b < 0) {
-        negB = true
-        b = Math.abs(b)
-    }
-    let results = 0;
-    let count = 0;
-    while (count < b) {
-        results += a
-        count++
-    }
-    if ((negA && negB) || (!negA && !negB)){
-        return results
-    }
-    return -results
-}
-
-const divide = (a, b) => {
-    let negA = false
-    let negB = false
-    if (a < 0) {
-        negA = true
-        a = Math.abs(a)
-    }
-    if (b < 0) {
-        negB = true
-        b = Math.abs(b)
-    }
-
-    let results = a;
-    let count = 0;
-
-    while (results >= b) {
-        results -= b
-        count++
-    }
-
-    if ((negA && negB) || (!negA && !negB)) {
-        return count
-    }
-    return -count
-}
-
-function modulo(num, divisor) {
-    return num - multiply(divisor, divide(num, divisor));
-}
-
 const round = (n) => {
-    let isNeg
+    let neg = false;
     if (n < 0) {
-        n = -n
-        isNeg = true
+        neg = true;
+        n = -n;
     }
+    let num = 0;
+    while (!(n < 1 && n > -1)) {
+        n -= 1;
+        num++;
+    }
+    if (n < 0.5) {
+        if (neg) {
+            return -num;
+        } else {
+            return num;
+        }
+    } else {
+        if (neg) {
+            return -num - 1;
+        } else {
+            return num + 1;
+        }
+    }
+}
 
-    let mod = modulo(n, 1)
-    let num = n - mod
-    if (mod >= 0.5) {
-        num = num + 1
+function floor(n) {
+    let neg = false;
+    if (n < 0) {
+        neg = true;
+        n = -n;
     }
-    if (isNeg) {
+    let nr = n;
+    let num = 0;
+    while (!(nr < 1 && nr > -1)) {
+        nr -= 1;
+        num++;
+    }
+    if (neg) {
+        return -num - 1;
+    } else {
+        return num;
+    }
+}
+
+function ceil(n) {
+    if (!n) return 0;
+    let neg = false;
+    if (n < 0) {
+        neg = true;
+        n = -n;
+    }
+    let nr = n;
+    let num = 0;
+    while (!(nr < 1 && nr >= 0)) {
+        nr -= 1;
+        num++;
+    }
+    if (neg) {
         if (num === 0) {
-            num = num
-        } else {
-            num = -num
+            return num
         }
+        return -num;
+    } else {
+        return num + 1;
     }
-    return num
 }
 
-const ceil = (n) => {
-    let isNeg
+function trunc(n) {
+    let num = 0;
+    if (n > 0xfffffffff) {
+        n -= 0xfffffffff;
+        num += 0xfffffffff;
+    }
+    let neg = false;
     if (n < 0) {
-        n = -n
-        isNeg = true
+        neg = true;
+        n = -n;
     }
-
-    let mod = modulo(n, 1)
-    let num = n - mod
-    if (mod > 0) {
-        if (isNeg) {
-            if (num === 0) {
-                num = num
-            } else {
-                num = -num
-            }
-        } else {
-            num += 1
-        }
+    let nr = n;
+    while (!(nr < 1 && nr > -1)) {
+        nr -= 1;
+        num++;
     }
-    return num
-}
-
-const floor = (n) => {
-    let isNeg = false
-    if (n < 0) {
-        n = -n
-        isNeg = true
-    }
-    let mod = modulo(n, 1)
-    let num = n - mod
-
-    if (mod > 0) {
-        if (isNeg) {
-            num += 1
-            if (num === 0) {
-                num = num
-            } else {
-                num = -num
-            }
-        } else {
-            num = num
-        }
-    }
-    return num
-}
-
-const trunc = (n) => {
-    let isNeg = false
-    if (n < 0) {
-        isNeg = true
-        n = -n
-    }
-
-    let mod = modulo(n, 1)
-    let num = n - mod
-    if (isNeg) {
+    if (neg) {
         if (num === 0) {
-            num = num
-        } else {
-            num = -num
+            return num
         }
+        return -num;
     }
-    return num
+    return num;
 }
 
-// console.log(round(-5.6004))
-// console.log(round(-0.6004))
-// console.log(round(-0.01))
-// console.log(ceil(-5.6004))
-// console.log(ceil(0.6004))
-// console.log(ceil(0.0))
-// console.log(floor(0.0))
-// console.log(floor(0.05))
-// console.log(floor(5.05))
-// console.log(floor(5.05))
-// console.log(floor(-1.01))
-// console.log(trunc(0.01))
-// console.log(trunc(-0.01))
-// console.log(trunc(-6.01))
-// console.log(trunc(-7.91))
-// console.log(trunc(3.91))
+
+// console.log(trunc(-0.435))
