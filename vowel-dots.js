27b881d470ad4f118dacf391d31747ae6da5cb97
Baraq23 <wolfgangkoppe18@gmail.com>
2024-08-31 23:49:57 +0300

    feat: function vowelDot that puts a dot after every vowel of a string

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,8 +1,9 @@
 const vowelDots = (s) => {
-    let vowels = /(a|e|i|o|u)/;
     let n = s.replace(/(a|e|i|o|u)/g, "$&.")
     return n
 }
+let vowels = /(a|e|i|o|u)/;
+
 
 // let text = "The cat is on the mat. It took a lot of time. Why try?";
 // console.log(vowelDots(text))
