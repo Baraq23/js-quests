6d0c0a3fe0b5b3b4678242dee94845c9c303debd
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-01 06:25:30 +0300

    feat: function to compaire two regex results.

--- same-amount.js
+++ same-amount.js
@@ -1,7 +1,9 @@
 const sameAmount = (s, r1, r2) => {
+    r1 = new RegExp(r1, 'g')
+    r2 = new RegExp(r2, 'g')
     let s1 = s.match(r1);
     let s2 = s.match(r2);
-    if (s1.length === s2.length) {
+    if (si !== null && s2 !== null && s1.length === s2.length) {
         return true
     }
     return false
