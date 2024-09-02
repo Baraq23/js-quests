2fa00cb4a64bf6db7edc28c0c98006df94121880
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-02 19:29:37 +0300

    feat: that gets the day of the week given date

---
+++ dr-strange.js
@@ -0,0 +1,26 @@
+const addWeek = (date) => {
+    const weekday = [
+        "Monday",
+        "Teusday",
+        "Wednessday",
+        "Thursday",
+        "Friday",
+        "Sarurday",
+        "Sunday",
+        "secondMonday",
+        "secondTeusday",
+        "secondWednessday",
+        "secondThursday",
+        "secondFriday",
+        "secondSarurday",
+        "secondSunday"
+    ]
+    
+    git
+}
+
+console.log(addWeek(new Date('0001-01-01')))
+
+const timeTravel = () => {
+    
+}
