2165d5942d3329f30ecf432f694fcaa2c78229e7
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-03 16:46:18 +0300

    feat: function to chech a valid date instance

---
+++ date-is.js
@@ -0,0 +1,48 @@
+
+const isValid = (a) => {
+    if (isNaN((new Date(a)))) {
+        return false
+    }
+    return true
+}
+
+const isAfter = (a,b) => {
+    var tA = new Date(a).getTime()
+    var tB = new Date(b).getTime()
+    return tA > tB
+}
+
+const isBefore = (a,b) => {
+    var tA = new Date().getTime()
+    var tB = new Date().getTime()
+    return tA < tB
+}
+
+const isFuture = (a) => {
+    if (isValid(new Date(a))) {
+        var tA = new Date(a).getTime()
+        var today = new Date().getTime()
+        return today < tA
+    }
+    return false
+}
+
+const isPast = (a) => {
+    if (isValid(new Date(a))) {
+        var tA = new Date(a).getTime()
+        var today = new Date().getTime()
+        return today > tA
+    }
+    return false
+}
+
+
+
+// console.log(isValid("0010 1 07"))
+// console.log(isFuture("2025 01 01"))
+// console.log(isValid("tuesday"))
+
+// console.log(isValid("0001-02-03"))
+// console.log(new Date('today'))
+// console.log(isBefore("0001-01-09" , "0001-01-02"))
+
