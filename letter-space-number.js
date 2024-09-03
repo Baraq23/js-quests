22d67a92456f2fdea3f94ed3a6b01afae4590426
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-03 14:55:20 +0300

    feat: that gets the day of the week given date

--- letter-space-number.js
+++
@@ -1,7 +0,0 @@
-const letterSpaceNumber = (s) => {
-    let reg = /[a-zA-Z]\s[0-9]{1}(?![a-zA-Z0-9])/g
-    let matches = s.match(reg)
-    return matches || []
-}
-
-// console.log(letterSpaceNumber('example 1, example 20'))
