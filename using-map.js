0c21e038c9f10961e39b65b1797b0670d7f3900b
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-05 13:08:14 +0300

    feat: using-msp.js file to understand the use of mapas in js

---
+++ using-map.js
@@ -0,0 +1,96 @@
+const citiesOnly = (arr) => {
+    const map = arr.map((x) => x.city)
+    return map
+}
+
+
+const upperCasingStates = (arr) => {
+    const map = arr.map((x) => {
+        let item = x.split(' ')
+        let nArr = []
+        for (let i = 0; i < item.length; i++) {
+            let ns = ''
+        ns = item[i].charAt(0).toUpperCase() + item[i].slice(1)
+        nArr.push(ns)
+        }
+        let nItem = nArr.join(' ')
+        return nItem
+        
+    })
+    return map
+}
+
+const fahrenheitToCelsius = (arr) => {
+    const map = arr.map((x) => {
+        let num = Number(x.slice(0,-2))
+        let newStr = String(Math.round((num-32)/1.8)) + x.slice(-2)
+       return newStr
+
+    })
+    return map
+}
+
+const trimTemp = (obj) => {
+    let map = obj.map((x) => {
+        x.temperature = x.temperature.split(' ').join('')        
+        return x
+    })
+    return map
+}
+
+
+const fToC = (n) => {
+    let t = Number(n.slice(0,-2))
+    let temp = Math.round((t-32)/1.8)
+    return temp
+}
+
+const toCap = (s) => {
+    let sl = s.split(' ')
+    let ns = []
+    for (let i = 0; i < sl.length; i++) {
+        let str = sl[i].charAt(0).toUpperCase() + sl[i].slice(1)
+        ns.push(str)
+    }
+    return ns.join(' ')
+}
+
+const tempForecasts = (obj) => {
+    const map = obj.map((x) => {
+        let tmp = x.temperature.split(' ').join('')
+        let temp = fToC(tmp)
+        let st = toCap(x.state)
+        let str = temp +'°Celsius in '+ x.city + ', ' + st
+        return str
+
+    })
+    return map
+}
+
+// console.log(tempForecasts([
+//     {
+//       city: 'Pasadena',
+//       temperature: ' 101 °F',
+//       state: 'california',
+//       region: 'West',
+//     },
+//     {
+//     city: 'Kisumu',
+//     temperature: ' 161 °F',
+//     state: 'Kenya',
+//     region: 'Nyanza',
+//     }
+//   ]))
+
+// console.log(trimTemp([
+//     {
+//       city: 'Pasadena',
+//       temperature: ' 101 °F',
+//       state: 'california',
+//       region: 'West',
+//     },
+//   ]))
+
+  
+
+
