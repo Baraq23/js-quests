c58c94e5a6376c0adbf864c78ca630f66f8c48ec
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-05 20:42:16 +0300

    add filter.js

---
+++ filter.js
@@ -0,0 +1,29 @@
+const filter = (arr, func) => {
+    let arr1 = []
+    for (let i = 0 ; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) {
+            arr1.push(arr[i])
+        }
+    }
+    return arr1
+}
+
+const reject = (arr, func) => {
+    let arr1 = []
+    for (let i = 0; i < arr.length; i ++) {
+        if (!(func(arr[i], i, arr))) {
+            arr1.push(arr[i])
+        }
+    }
+    return arr1
+}
+
+const partition = (arr, func) => {
+    let a = []
+    let arr1 = filter(arr, func)
+    let arr2 = reject(arr, func)
+   
+    a.push(arr1)
+    a.push(arr2)
+    return a
+}
