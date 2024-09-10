35eecd1e4cc6a9047b9ead75a6cf975bd82c2e6c
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-10 14:50:10 +0300

    add: invert.js feat: contain func that inverts objects

--- pick-omit.js
+++ pick-omit.js
@@ -16,21 +16,29 @@ const pick = (obj, arr) => {
     return newObj
 }
 
-const omit = (obj, s) => {
+const omit = (obj, arr) => {
     let newObj = {}
     let keys = Object.keys(obj)
-    let arr
-    Array.isArray(arr) ? arr = [...s] : arr = [s]
     if (arr instanceof Array) {
         for (let i = 0; i < arr.length; i++) {
-            if (keys.includes(arr[i])) {
-                continue
+            if (!keys.includes(arr[i])) {
+                newObj[arr[i]] = obj[arr[i]]
             }
-            newObj[arr[i]] = obj[arr[i]]
+        }
+    }
+    if (typeof arr == "string") {
+        if (!keys.includes(arr)) {
+            newObj[arr] = obj[arr]
         }
     }
     return newObj
 }
 
 
-// console.log(omit({ something: 5, __proto__: { d: 6 } }, 'something'))
+// console.log(omiti("hello"))
+// console.log(omiti(['something', "sm"]))
+// console.log(omiti(['something']))
+
+
+// console.log(omit({ something: 5, sm: { d: 6 } }, ['something', "sm"]))
+// console.log(omit({ something: 5, sm: { d: 6 } }, 'sm'))
