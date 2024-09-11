42aa722ce1f2795d36a62ba032c1093d4ec7607f
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-11 16:12:51 +0300

    feat: functions that works like the .filter, .map and .reduce array methods, but for the keys

---
+++ manipulate-keys.js
@@ -0,0 +1,46 @@
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+
+const filterKeys = (obj, f) => {
+    let newObj = {}
+    for (let key in obj) {
+        if (f(key)){
+            newObj[key] = obj[key]
+        }
+    }
+    return newObj
+}
+
+const mapKeys = (obj, f) => {
+    let newObj = {}
+    for (let key in obj) {
+        let newKey = f(key)
+        newObj[newKey] = obj[key]
+    }
+    return newObj
+}
+
+const reduceKeys = (obj, f) => {
+    let str = ""
+    let i = 0
+    for (let key in obj) {
+        if (i===0) {
+            str = key
+            i++
+            continue            
+        }
+        str = f(str, key)
+        i++
+    }
+    return str
+}
+
+
+
+// console.log(reduceKeys(nutrients, (acc, cr) =>acc.concat(', ', cr)))
+// output: carbohydrates, protein, fat
+
+// console.log(mapKeys(nutrients, (k) => `-${k}`))
+// output: { -carbohydrates: 12, -protein: 20, -fat: 5 }
+
+// console.log(filterKeys(nutrients, (key) => /protein/.test(key)))
+// output: { protein: 20 }
