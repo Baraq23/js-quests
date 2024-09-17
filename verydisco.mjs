48018d2013d4fe5f2b79bd5e62b3d9f5db5ad484
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-17 16:44:04 +0300

    add verydisco.mjs

---
+++ verydisco.mjs
@@ -0,0 +1,39 @@
+// console.log(process.argv[2])
+
+
+let word = process.argv[2]
+// console.log("half", half)
+let oarr = word.split(" ")
+let narr = []
+
+
+
+for (let i = 0; i < oarr.length; i++) {
+    let w = oarr[i]
+    let arr = []
+    let half = Math.ceil(w.length / 2)
+
+
+    for (let i = 0; true; i++) {
+        if (w.length == 0) {
+            break
+        }
+        if (w.length > half) {
+            arr.push(w.slice(0, half))
+            w = w.slice(half)
+        }
+        if (w.length <= half) {
+            arr.push(w.slice(0, half))
+            w = ''
+        }
+    }
+    let nStr = ""
+    for (let i = arr.length - 1; i >= 0; i--) {
+        nStr += arr[i]
+    }
+    narr.push(nStr)
+}
+
+let newStr = narr.join(' ')
+// console.log(nStr)
+console.log(newStr)
