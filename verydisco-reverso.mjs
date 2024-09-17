6107c78be78f68282d0e696ba44df91f172c4461
Baraq23 <wolfgangkoppe18@gmail.com>
2024-09-17 18:03:43 +0300

    add verydisco-reverse.mjs

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -11,7 +11,7 @@ try {
 }
 
 
-function reverse(word){
+function reverse(word) {
 
     let oarr = word.split(" ")
     let narr = []
@@ -21,22 +21,12 @@ function reverse(word){
     for (let i = 0; i < oarr.length; i++) {
         let w = oarr[i]
         let arr = []
-        let half = Math.ceil(w.length / 2)
+        let half = Math.floor(w.length / 2)
 
+        arr.push(w.slice(0, half))
+        w = w.slice(half)
+        arr.push(w)
 
-        for (let i = 0; true; i++) {
-            if (w.length == 0) {
-                break
-            }
-            if (w.length > half) {
-                arr.push(w.slice(0, half))
-                w = w.slice(half)
-            }
-            if (w.length <= half) {
-                arr.push(w.slice(0, half))
-                w = ''
-            }
-        }
         let nStr = ""
         for (let i = arr.length - 1; i >= 0; i--) {
             nStr += arr[i]
